42071b742036c3fca03cdf3a91bc2760755eec17
Bogac Bokeer <itsme@local>
2016-04-15 21:09:08 +0300

    Added support nested accordions

--- dist/jquery.bbAccordion.js
+++ dist/jquery.bbAccordion.js
@@ -1,17 +1,43 @@
 /**
  * jquery.bbAccordion.js
- * version 1.1.1
+ * version 1.2.0
  * me@bokeer.net
  *
- * Copyright 2013-2015
+ * Copyright 2013-2016
  * Free to use under the MIT license.
  *
  * Date: Jan 15, 2014
  */
 !(function($) {
+
+    $.bbAccordion = {
+        close: function(el, settings) {
+
+            var that = el,
+                $that = $(that),
+                $content = $('>' + settings.divContent, $that);
+
+            if ( $that.hasClass(settings.classActive) ) {
+                $that.removeClass(settings.classActive);
+                $content.slideUp(settings.animSpeed);
+            };
+        },
+        open: function(el, settings) {
+
+            var that = el,
+                $that = $(that),
+                $content = $('>' + settings.divContent, $that);
+
+            if ( !$that.hasClass(settings.classActive) ) {
+                $that.addClass(settings.classActive);
+                $content.slideDown(settings.animSpeed);
+            };
+        }
+    };
+
     $.fn.bbAccordion = function(options) {
 
-    	var settings = $.extend({},{
+    	var settings = $.extend({
             selectedIdx: false,
             classActive: 'active',
             selectorGroup: 'accordionGroup',
@@ -20,13 +46,15 @@
             animSpeed: 500
     	}, options);
 
+        var root = $.bbAccordion;
+
         return this.each(function() {
 
             var that = this,
                 $that = $(this),
                 data = $that.data(),
-                $head = $(settings.divHead, $that),
-                $content = $(settings.divContent, $that),
+                $head = $('>' + settings.divHead, $that),
+                $content = $('>' + settings.divContent, $that),
                 group = data.accordionGroup || null,
                 groupItems = group ? $('[data-accordion-group="' + group + '"]') : null;
 
@@ -45,19 +73,34 @@
                 e.preventDefault();
 
                 if ( $that.hasClass(settings.classActive) ) {
-                    $that.removeClass(settings.classActive);
-                    $content.slideUp(settings.animSpeed);
+                    $that.trigger('close.bbAccordion');
                     return;
                 }
 
                 if ( groupItems ) {
-                    groupItems.filter("." + settings.classActive).each(function() {
-                        $(settings.divContent, $(this)).slideUp(settings.animSpeed);
-                    }).removeClass(settings.classActive);
+                    groupItems.filter("." + settings.classActive)
+                              .trigger('close.bbAccordion')
+                              .removeClass(settings.classActive);
                 }
 
-                $that.addClass(settings.classActive);
-                $content.slideDown(settings.animSpeed);
+                $that.find('.bbAccordion').trigger('close.bbAccordion');
+
+                setTimeout(function() {
+                    root.open(that, settings);
+                }, 0);
+            });
+
+            $that.on('close.bbAccordion', function(e) {
+
+                e.stopPropagation();
+
+                root.close(this, settings);
+
+            }).on('open.bbAccordion', function(e) {
+
+                e.stopPropagation();
+
+                root.open(this, settings);
             });
         });
     };
